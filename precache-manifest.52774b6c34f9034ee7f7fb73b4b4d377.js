10b3a9ef5a21687c9014feea45aa6aeef35d8e8b
Luis Campos <lucam75@gmail.com>
2019-05-08 08:59:43 -0400

    Updates

---
+++ precache-manifest.52774b6c34f9034ee7f7fb73b4b4d377.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b005e1fd9d63be1c5f6fb12ffcc8429e",
+    "url": "/reactjs-tutorial/index.html"
+  },
+  {
+    "revision": "0b1cd791a60d564b4a13",
+    "url": "/reactjs-tutorial/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "f2bf82953ccd6cf2478e",
+    "url": "/reactjs-tutorial/static/js/2.087e00c1.chunk.js"
+  },
+  {
+    "revision": "0b1cd791a60d564b4a13",
+    "url": "/reactjs-tutorial/static/js/main.35593bf8.chunk.js"
+  },
+  {
+    "revision": "45f280820350a34d4757",
+    "url": "/reactjs-tutorial/static/js/runtime~main.783afb84.js"
+  }
+]);
